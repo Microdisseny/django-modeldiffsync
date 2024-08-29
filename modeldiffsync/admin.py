30bcbcf6fb7e15665af7d6794dcd3897fbc4d8bf
Jordi Gomara Cruz <jordigomaracruz@gmail.com>
2024-08-29 11:27:03 +0200

    fix django urls

--- modeldiffsync/admin.py
+++ modeldiffsync/admin.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
 from django.forms import TextInput, Textarea
 from django.http import HttpResponseRedirect
@@ -74,7 +74,7 @@ class ModeldiffSyncAdmin(admin.ModelAdmin):
     def get_urls(self):
         urls = super(ModeldiffSyncAdmin, self).get_urls()
         my_urls = [
-            url(r'^apply_modeldiffs/$',
+            re_path(r'^apply_modeldiffs/$',
                 self.admin_site.admin_view(self.apply_modeldiffs))
         ]
         return my_urls + urls
